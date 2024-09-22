e88ca776721eef166dff3ce65ffc65028538d482
elephantmagnum <abhimanyujangir2211@gmail.com>
2024-09-23 00:22:14 +0530

    Added no results page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import ContactCard from '@/components/ContactCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 interface Person {
   first_name: string;
@@ -34,19 +35,34 @@ const SearchResults: React.FC = () => {
   }, [query]);
 
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-t from-blue-800 to-white">
+    <div className="min-h-screen flex flex-col bg-gradient-to-t from-blue-200 to-white">
       {/* Navbar at the top */}
       <Navbar isSearchPage={true} />
 
       {/* Main content area */}
-      <div className="flex-grow w-full max-w-6xl mx-auto px-4 py-8">
-        {/* Cards container */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {results.map((person, index) => (
-            <ContactCard key={index} person={person} />
-          ))}
-        </div>
-      </div>
+
+      {
+        results.length === 0 ?
+          (
+            <div className="flex flex-grow items-center justify-center">
+              <div className="text-center">
+              <Image src="/sorry.png" alt="No results" width={400} height={400} />
+              <p className="text-gray-500">No results found</p>
+              </div>
+            </div>
+          )
+          :
+          (
+            <div className="flex-grow w-full max-w-6xl mx-auto px-4 py-8">
+              {/* Cards container */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {results.map((person, index) => (
+                  <ContactCard key={index} person={person} />
+                ))}
+              </div>
+            </div>
+          )
+      }
     </div>
   );
 };
